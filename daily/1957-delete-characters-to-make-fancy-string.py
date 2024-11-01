5d2c920ac79cbafd816cedf5ac3c377d4ac163c9
hengtseChou <hankthedev@gmail.com>
2024-11-01 22:18:38 +0800

    add: daily question 2024-11-01

---
+++ daily/1957-delete-characters-to-make-fancy-string.py
@@ -0,0 +1,19 @@
+class Solution:
+    def makeFancyString(self, s: str) -> str:
+        
+	ans = []
+        last_char = ""
+        count = 0
+        for char in s:
+            if char != last_char:
+                ans.append(char)
+                last_char = char
+                count = 1
+            else:
+                if count == 2:
+                    continue
+                else:
+                    ans.append(char)
+                    count +=1
+        return "".join(ans)
+        
