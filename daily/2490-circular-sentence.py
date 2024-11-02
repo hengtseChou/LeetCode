47d1b2eacbf61c762e0b1fd4f8d177ca6da592fc
hengtseChou <hankthedev@gmail.com>
2024-11-02 19:08:19 +0800

    add: daily question 2024-11-02

---
+++ daily/2490-circular-sentence.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isCircularSentence(self, sentence: str) -> bool:
+
+        if sentence[0] != sentence[-1]:
+            return False
+        for i, char in enumerate(sentence):
+            if char == " " and sentence[i - 1] != sentence[i + 1]:
+                return False
+        return True
